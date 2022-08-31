ab6c2c81ac275e6748b93f162a0465cc54469c34
Evans <evans.njeru@student.moringaschool.com>
2022-08-31 21:13:07 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	const submit = document.querySelector('input[type="submit"]');
+	submit.addEventListener("click", (e) => {
+		// 1. Create list element
+		// a. Create task
+		const newTask = document.getElementById("new-task-description");
+		const tasks = document.getElementById("tasks");
+		const li = document.createElement("li");
+		const task = document.createTextNode(newTask.value);
+		li.appendChild(task);
+
+		// b. Create button for removing tasks
+		const button = document.createElement("button");
+		button.innerHTML = "X";
+		button.addEventListener("click", () => {
+			tasks.removeChild(li);
+		});
+		li.appendChild(button);
+
+		// 2. Add task to list
+		tasks.appendChild(li);
+
+		// 3. Disable default behaviour for submit
+		e.preventDefault();
+	});
 });
